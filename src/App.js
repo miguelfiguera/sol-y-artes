2fccc861cdb038dccbaca1732f3ab9568227be11
Miguel Figuera <miguelqui725@gmail.com>
2023-07-02 00:19:11 -0400

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import { Footer } from "./components/footer";
 import { useState } from "react";
 import { Teachers } from "./components/teachers/teachers";
 import { Classes } from "./components/classes/classes";
+import Canto from "./components/classes/Canto";
+import Instrumentos from "./components/classes/Instrumentos";
+import Ensambles from "./components/classes/Ensambles";
+import IniciacionMusical from "./components/classes/IniciacionMusical";
+import PreyPostnatal from "./components/classes/PreyPostnatal";
+import TeoriaYArmonia from "./components/classes/TeoriaYArmonia";
+import Conciertos from "./components/classes/Conciertos";
 
 function App() {
   const [modal, setModal] = useState(false);
@@ -24,6 +31,13 @@ function App() {
           <Route exact path="/sol-y-artes/galery" Component={Galery} />
           <Route exact path="/sol-y-artes/teachers" Component={Teachers} />
           <Route exact path="sol-y-artes/clases" Component={Classes} />
+          <Route exact path="sol-y-artes/clases/canto" Component={Canto}/>
+          <Route exact path="sol-y-artes/clases/conciertos" Component={Conciertos}/>
+          <Route exact path="sol-y-artes/clases/ensambles" Component={Ensambles}/>
+          <Route exact path="sol-y-artes/clases/iniciacion-musical" Component={IniciacionMusical}/>
+          <Route exact path="sol-y-artes/clases/instrumentos" Component={Instrumentos}/>
+          <Route exact path="sol-y-artes/clases/pre-y-postnatal" Component={PreyPostnatal}/>
+          <Route exact path="sol-y-artes/clases/teoria-y-armonia" Component={TeoriaYArmonia}/>
         </Routes>
         <Contact open={modal} closeModal={() => setModal(false)} />
         <Footer />
---
+++ src/components/classes/Instrumentos.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Instrumentos() {
+  return (
+    <div className="container">
+      <img src="" alt="" />
+
+      <h1>Instrumentos</h1>
+      <div className="row">
+        <p>
+          {" "}
+          Sin límite de edad, aprender a ejecutar un instrumento musical puede
+          estimular el cerebro de gran manera: incrementa la materia gris,
+          mejora las conexiones entre las áreas más distanciadas del cerebro y
+          mejora la memoria verbal, el razonamiento espacial y las habilidades
+          de aprendizaje. A su vez, brinda organización que es necesario para la
+          vida diaria. Ofrecemos clase de guitarra acústica o eléctrica, bajo
+          eléctrico, batería, piano o teclado y violín. El contenido se
+          distingue por la enseñanza de la teoría musical aplicada al
+          instrumento, recursos técnicos interpretativos que facilitarán el
+          mecanismo de la ejecución del instrumento y el repertorio según el
+          interés del alumno y las recomendaciones del docente. A su vez, podrán
+          participar en los ensambles y conciertos organizados por la escuela.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
